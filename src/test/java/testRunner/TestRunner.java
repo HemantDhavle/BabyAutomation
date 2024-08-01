a893645b5eb2ade0a7cb85faa89129693c09ff00
hemant <hemantd17@gmail.com>
2024-08-01 19:02:21 +0530

    adding change code into master branch

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,7 +3,11 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+<<<<<<< Updated upstream
 @CucumberOptions(features="src\\test\\java\\features",glue="stepDefinations",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+=======
+@CucumberOptions(features="src\\test\\java\\features",monochrome=true,glue="stepDefinations",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+>>>>>>> Stashed changes
 public class TestRunner  extends AbstractTestNGCucumberTests
 {
 
