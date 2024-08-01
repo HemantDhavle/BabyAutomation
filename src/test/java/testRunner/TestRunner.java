cc8030d2e562468880822fef9122d83ccb40762e
hemant <hemantd17@gmail.com>
2024-08-01 19:05:45 +0530

    test

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,11 +3,8 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-<<<<<<< Updated upstream
-@CucumberOptions(features="src\\test\\java\\features",glue="stepDefinations",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-=======
+
 @CucumberOptions(features="src\\test\\java\\features",monochrome=true,glue="stepDefinations",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
->>>>>>> Stashed changes
 public class TestRunner  extends AbstractTestNGCucumberTests
 {
 
